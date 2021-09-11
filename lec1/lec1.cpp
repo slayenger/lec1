dd1815713a5795ccb3ec3b81d831227a638a2bd4
slayenger <sinyavskiym@mail.ru>
2021-09-11 18:28:58 +0300

    task1

---
+++ lec1/lec1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int numvar;
+	cout << "Enter the number of variants" << endl;
+	cin >> numvar;
+
+	string name;
+	cout << "Enter a name" << endl;
+	cin >> name;
+
+	int var = name[0] % numvar + 1; //name[0] - code of the 1st letter in ASCII
+	cout << "Your variant - " << var;
+}
